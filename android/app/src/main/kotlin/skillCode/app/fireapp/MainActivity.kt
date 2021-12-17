445c703a443e20cd54e1ffe9e7078aa4357498e8
skillqode <studentskillqode>
2021-12-17 11:51:18 +0530

    first commit

---
+++ android/app/src/main/kotlin/skillCode/app/fireapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package skillCode.app.fireapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ fireappn/android/app/src/main/kotlin/skillCode/fireapp/fireappn/MainActivity.kt
@@ -0,0 +1,6 @@
+package skillCode.app.fireappn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
